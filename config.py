dccda1590173ba1c0b508c868817d5abd5849f4d
CrePavan <93823376+CrePavan@users.noreply.github.com>
2022-02-16 21:11:04 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@
 BOT_TOKEN = "5085381545:AAHzAlhunwwb5f7Mr5WCcSkvs7hA7wUE5cw"
 
 # Project Name of your Project
-PROJECT_NAME = "GitGram"
+PROJECT_NAME = "GitHubNotification"
 
 # If you forked the Repository, grab your repo's link and paste it below!
-GIT_REPO_URL = "https://github.com/AnonymousBoy1025/GitGram"
+GIT_REPO_URL = "https://github.com/CrePavan/GitHubNotification"
